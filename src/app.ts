ef70a59690659053506fcdbc3fc1a7daf3e19cc2
gegham1 <gegham99999@gmail.com>
2022-06-07 01:25:21 +0400

    documented the api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
+import { resolve } from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import rideController from './controllers/RideController';
 import validatePostRideBody from './validators/validatePostRideBody';
 import validateParamId from './validators/validateParamId';
 
 const app = express();
 const jsonParser = bodyParser.json();
+const appVersion = process.env.VERSION;
+const docVersion = `v${appVersion ? appVersion[0] : 1}`;
+const swaggerDocument = YAML.load(resolve(`docs/api/${docVersion}/main.yaml`));
 
 app.use(jsonParser);
 app.use(helmet());
@@ -15,5 +21,6 @@ app.get('/health', (req, res) => res.send('Healthy'));
 app.get('/rides', rideController.getAllRides);
 app.post('/rides', validatePostRideBody, rideController.createRide);
 app.get('/rides/:id', validateParamId, rideController.getById);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
